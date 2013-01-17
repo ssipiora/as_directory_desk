b1ef8c8484a1deef5be741b6f3849a434d6dc1ba
ssipiora <ssipiora@gmail.com>
2013-01-17 09:41:49 -0600

    got dynamic loading of accordion working for each record

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,3 @@
 class Person < ActiveRecord::Base
-  attr_accessible :address, :capacity, :city, :column, :country, :department, :email, :equipment, :fname, :lname, :ntid, :phone, :state, :suite, :title, :zipcode
+  attr_accessible :address, :capacity, :city, :column, :country, :department, :email, :equipment, :fname, :lname, :ntid, :phone, :state, :suite, :title, :zipcode, :manager, :direct_report, :mobile, :pager, :fax, :assistant, :company, :office 
 end
